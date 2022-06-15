f69aae1b00b2bf4b57b8d21350522fa80677d8b1
ChanYoungHong <cksdud9883@naver.com>
2022-06-15 14:36:38 +0900

    2468 복습 답이 5인데 계속 10이 나온다 확인

--- src/main/java/problem2/BOJ_2468dfs.java
+++ src/main/java/problem2/BOJ_2468dfs.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 // 문제에서 봐야 할 point
 // 4방향에서 가장 큰 숫자를 골라야 한다.
@@ -14,11 +15,13 @@ public class BOJ_2468dfs {
     // 행과 열의 나타내는 지도 N
     static int N;
     static int[][] map;
+    static int[][] transmap;
     static boolean[][] visited;
 
-    static ArrayList<Integer> result;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
+    static int count;
+    static int Max;
 
     public static void main(String[] args) throws IOException {
 
@@ -26,25 +29,59 @@ public class BOJ_2468dfs {
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
-        visited = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
+//            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 // String -> char형으로 바꾸는 것
-                map[i][j] = input.charAt(j) - '0';
+//                map[i][j] = input.charAt(j) - '0';
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        result = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        Max = 1;
 
-                if(map[i][j] == 1 && !visited[i][j] {
+        for (int z = 0; z < 101; z++) {
 
+            transmap = new int[N][N];
+            count = 0;
+
+            visited = new boolean[N][N];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(map[i][j] <= i){
+                        transmap[i][j] = 0;
+                    }
+                    else transmap[i][j] = 1;
                 }
+            }
 
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(transmap[i][j] == 1 && !visited[i][j]) {
+                        dddosearch(i,j);
+                        count++;
+                    }
+                }
+            }
+            Max = Math.max(Max, count);
+        }
+        System.out.println(Max);
+    }
 
+    private static void dddosearch(int i, int j) {
+        visited[i][j] = true;
+
+        for (int k = 0; k < 4; k++) {
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+
+            if(nx > 0 && ny > 0 && nx < N && ny < N) {
+                if(transmap[nx][ny] == 1 & !visited[nx][ny]) {
+                    dddosearch(nx,ny);
+                }
             }
         }
     }
