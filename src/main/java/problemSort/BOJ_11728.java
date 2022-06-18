181859f3fa3c20cda80709b47484d38514eb10d5
ChanYoungHong <cksdud9883@naver.com>
2022-06-18 14:01:08 +0900

    Merge Sort 정렬 방식 BOJ 11728

---
+++ src/main/java/problemSort/BOJ_11728.java
@@ -0,0 +1,38 @@
+package problemSort;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11728 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]), m = Integer.parseInt(input[1]);
+
+        int size = n + m;
+        int[] result = new int[size];
+
+        String[] arrayA = br.readLine().split(" ");
+        String[] arrayB = br.readLine().split(" ");
+
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            result[idx++] = Integer.parseInt(arrayA[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            result[idx++] = Integer.parseInt(arrayB[i]);
+        }
+        Arrays.sort(result);
+
+        for (int y : result) bw.write(String.valueOf(y + " "));
+
+        bw.flush();
+    }
+}
