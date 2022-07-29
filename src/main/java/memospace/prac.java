d1aed14edbe4406a6622c03f23eb285ecf4a07e7
ChanYoungHong <cksdud9883@naver.com>
2022-07-29 19:39:50 +0900

    자료구조 Queue 코드 연습해보기.

--- src/main/java/memospace/prac.java
+++ src/main/java/memospace/prac.java
@@ -1,34 +1,43 @@
 package memospace;
 
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class prac {
 
-    static boolean[] arr;
-
     public static void main(String[] args) {
 
-        prac k = new prac();
-        System.out.println(k.hihi(10));
+        Queue<Message> messageQueue = new LinkedList<>();
 
-    }
+        messageQueue.offer(new Message("sendMail", "홍길동"));
+        messageQueue.offer(new Message("sendSMS", "신용권"));
+        messageQueue.offer(new Message("sendKakaotalk", "홍두께"));
 
-    private boolean hihi(int x) {
-        boolean answer = false;
-
-        int y;
-        int sum;
-
-        y = x/10;
-        int z = x%10;
-
-        sum = (y+z);
-
-        if(x%sum == 0){
-           answer = true;
+        while (!messageQueue.isEmpty()) {
+            Message message = messageQueue.poll(); //
+            switch (message.command) {
+                case "sendMail":
+                    System.out.println(message.to + "님에게 메일을 보냅니다");
+                    break;
+                case "sendSMS":
+                    System.out.println(message.to + "님에게 SMS를 보냅니다.");
+                    break;
+                case "sendKakaotalk":
+                    System.out.println(message.to + "님에게 카카오톡을 보냅니다");
+                    break;
+            }
         }
+    }
+}
 
-        return answer;
+class Message {
+
+    public String command;
+    public String to;
+
+    public Message(String command, String to) {
+        this.command = command;
+        this.to = to;
     }
 
 }
