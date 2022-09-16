74f2a9122524effbc6e75ea3cd9391acc8dd7416
chanyoung Hong <cksdud9883@naver.com>
2022-09-16 19:52:33 +0900

    마지막 배열의 자릿수만 구하기 연습

--- src/main/java/spharoscodingtestprac/정렬은_지겨워.java
+++ src/main/java/spharoscodingtestprac/정렬은_지겨워.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class 정렬은_지겨워 {
     public static void main(String[] args) throws IOException {
@@ -11,16 +14,34 @@ public class 정렬은_지겨워 {
 //        ArrayList<Integer> originalList = new ArrayList<>();
         ArrayList<Integer> oddList = new ArrayList<>();
         ArrayList<Integer> evenList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
+        int N = sc.nextInt();
         int[] array = new int[N];
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(br.readLine());
-            System.out.println(i + " ");
+            array[i] = sc.nextInt();
+            System.out.print(array[i] + " ");
+
+            // 짝수일 때
+            if(array[i] % 2 == 0){
+
+                evenList.add(array[i]);
+                Collections.reverse(evenList);
+            } else { // 홀수일 때
+                oddList.add(array[i]);
+                Collections.sort(oddList, Collections.reverseOrder());
+            }
+
+            if(array.length%2 == 0){
+                array[i] = oddList.get(i);
+            } else {
+                array[i] = evenList.get(i);
+            }
+
+            System.out.print(array[i] + " ");
         }
+
 
 
 
