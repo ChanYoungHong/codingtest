39a0a54f49e82e511e45ee0341eea43314bc99aa
chanyoung Hong <cksdud9883@naver.com>
2022-09-23 20:11:01 +0900

    코딩테스트 연습 및 복습

--- src/main/java/problemDP/BOJ_14889.java
+++ src/main/java/problemDP/BOJ_14889.java
@@ -1,16 +1,77 @@
 package problemDP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_14889 {
 
-    public static void main(String[] args) {
+    static int N;
+    static int[][] array;
+    static boolean[] visit;
 
-        System.out.println("feature test 진행 중");
+    static int min = Integer.MAX_VALUE;
 
-        System.out.println("git myfeature test 2");
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("git 3번째 test 진행 중");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 4
 
-        System.out.println("hello hello test test");
+        array = new int[N][N]; // 4 4
+        visit = new boolean[N];
 
-        System.out.println("김승후 바보");
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <N; j++) {
+                array[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfssssss(0,0);
+        System.out.println(min);
+
+
+    }
+
+    private static void dfssssss(int depth, int a) {
+
+        if(depth == N/2) {
+            diffff();
+            return;
+        }
+
+        for (int i = a; i < N; i++) {
+            visit[i] =true; // 비어있음
+            dfssssss(depth+1, i+1);
+            visit[i] = false; // 들어있음
+        }
+
+    }
+
+    private static void diffff() {
+        int start = 0;
+        int link = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i+1; j < N; j++) {
+                if(visit[i] == true && visit[j] == true) {
+                    start += array[i][j];
+                    start += array[j][i];
+                } else if(visit[i] == false && visit[j] == false) {
+                    link += array[i][j];
+                    link += array[j][i];
+                }
+            }
+        }
+
+        int val = Math.abs(start - link);
+
+        if(val == 0){
+            System.out.println(val);
+            System.exit(0);
+        }
+
+        min = Math.min(min, val);
     }
 }
---
+++ src/main/java/problemDP/BOJ_2667.java
@@ -0,0 +1,9 @@
+package problemDP;
+
+public class BOJ_2667 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
