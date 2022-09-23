e5818fd31f908c7b7157d3e6c2c3c3400ca6812d
chanyoung Hong <cksdud9883@naver.com>
2022-09-23 09:36:24 +0900

    hello hello test test

--- src/main/java/problemDP/BOJ_14889.java
+++ src/main/java/problemDP/BOJ_14889.java
@@ -1,2 +1,9 @@
-package problemDP;public class BOJ_14889 {
+package problemDP;
+
+public class BOJ_14889 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello hello test test");
+    }
 }
