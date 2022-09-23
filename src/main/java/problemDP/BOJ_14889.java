6545694670670012e52929aac387afa7d195b430
chanyoung Hong <cksdud9883@naver.com>
2022-09-23 09:33:32 +0900

    test

--- src/main/java/problemDP/BOJ_14889.java
+++ src/main/java/problemDP/BOJ_14889.java
@@ -1,2 +1,12 @@
-package problemDP;public class BOJ_14889 {
+package problemDP;
+public class BOJ_14889 {
+
+    public static void main(String[] args) {
+
+        System.out.println("feature test 진행 중");
+
+        System.out.println("git myfeature test 2");
+
+        System.out.println("git 3번째 test 진행 중");
+    }
 }
